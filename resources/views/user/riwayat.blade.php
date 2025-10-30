8756389ee229520d66d24c6e12a152b1d55eb798
Ratu Rizki <ratu.rizki5939@student.unri.ac.id>
2025-10-30 15:32:58 +0700

    Edit code tombol batal pesanan

--- resources/views/user/riwayat.blade.php
+++ resources/views/user/riwayat.blade.php
@@ -68,6 +68,8 @@
                                 class="bg-red-600 hover:bg-red-700 text-white px-4 py-2 rounded text-sm">
                                 Batalkan
                             </button>
+                        </form>
+                        @endif
                     </div>
                 </div>
             @endforeach
